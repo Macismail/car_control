08d17f944c2b959ab7eec0b9e467cd6d5f263f20
macismail <samouel_77@hotmail.com>
2019-04-16 01:22:56 +0100

    add car control java class

--- src/main/java/client/CarControl.java
+++ src/main/java/client/CarControl.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package client;
 
 import com.google.protobuf.Empty;
@@ -5,46 +10,57 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.mycompany.example.car.CarGrpc;
 import org.mycompany.example.car.WindowStatus;
 
-public class CarClient implements ServiceObserver {
-
+/**
+ *
+ * @author ismail
+ */
+public class CarControl implements ServiceObserver {
    protected ServiceDescription current;
-   private final String serviceType;
-   private final String name;
-   private static final Logger logger = Logger.getLogger(GRPCCarClient.class.getName());
-
    private ManagedChannel channel;
    private CarGrpc.CarBlockingStub blockingStub;
+   private final String serviceType;
+   private final String name;
 
-   /**
-    * Constructor.
-    */
-   public CarClient() {
-      serviceType = "_windows._up.local.";
-      name = "Car";
+   private static final Logger logger = Logger.getLogger(CarControl.class.getName());
+
+   public CarControl() {
+      serviceType = "_car_control";
+      name = "car";
+      
       jmDNSServiceTracker clientManager = jmDNSServiceTracker.getInstance();
       clientManager.register(this);
 
       serviceAdded(new ServiceDescription("18.202.21.182", 50021));
    }
-
+   
    String getServiceType() {
       return serviceType;
    }
 
+   @Override
    public List<String> serviceInterests() {
       List<String> interests = new ArrayList<String>();
       interests.add(serviceType);
       return interests;
    }
+   
+   @Override
+   public boolean interested(String type) {
+      return serviceType.equals(type);
+   }
 
+   @Override
+   public String getName() {
+      return name;
+   }
+
+   @Override
    public void serviceAdded(ServiceDescription service) {
       System.out.println("Start service");
       current = service;
@@ -54,49 +70,24 @@ public class CarClient implements ServiceObserver {
       blockingStub = CarGrpc.newBlockingStub(channel);
       carControl();
    }
-
-   public boolean interested(String type) {
-      return serviceType.equals(type);
-   }
-
-   public String getName() {
-      return name;
-   }
-
-   public void shutdown() throws InterruptedException {
-      channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-   }
-
-   /**
-    * Close the car windows method.
-    */
-   public void carControl() {
+   
+   public void carControl(){
+      
       try {
-
-         new Thread() {
-            public void run() {
-               Empty request = Empty.newBuilder().build();
-
-               Iterator<WindowStatus> response = blockingStub.close(request);
-               while (response.hasNext()) {
-                  System.out.println(response.next().toString());
-               }
-            }
-         }.start();
-
+         
          Empty request = Empty.newBuilder().build();
-         WindowStatus status = blockingStub.getStatus(request);
-         System.out.println("Closing windows " + status);
-
-      } catch (RuntimeException e) {
-         logger.log(Level.WARNING, "RPC failed", e);
+         WindowStatus response = blockingStub.lockDoors(request);
+         System.out.println("Doors "+response);
+         
+      } catch (StatusRuntimeException e) {
+         logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
          return;
       }
-
+      
    }
-
+   
    public static void main(String[] args) {
-      new CarClient();
+      new CarControl();
    }
 
 }
